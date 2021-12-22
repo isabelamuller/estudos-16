6314bb4c100f1b01b5184eb13c7a8091f3c8ffab
Isabela Müller <isabelamuller1@hotmail.com>
2021-12-21 23:31:27 -0300

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        Integer[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int[] y = {3,6,2,9,5,7,3,2,1,11,19,15,17,4,8,10,14};
 
-        Arrays.sort(x, 2,14);
+        List<Integer> xList = Arrays.asList(x);
+        Collections.shuffle(xList);
+        Arrays.sort(y);
 
-        for(int i:x) {
+        for(int i:xList) {
             System.out.print(i + ", ");
         }
-
+        System.out.println(" ");
+        for(int i:y) {
+            System.out.print(i + ", ");
+        }
     }
 }
